3fe58b3d9807f7a139271b9db718ef6d97c4052a
Vladas Maier <vladas.mr@googlemail.com>
2020-07-21 21:57:00 +0200

    Improve German translation
    See: #TSK-61

--- app/src/main/java/com/vmaier/taski/utils/Utils.kt
+++ app/src/main/java/com/vmaier/taski/utils/Utils.kt
@@ -24,16 +24,16 @@ object Utils {
     fun getThemeByName(context: Context, theme: String?): Int {
         val resources = context.resources
         return when (theme) {
-            resources.getString(R.string.theme_default_name) -> R.style.Theme_Default
-            resources.getString(R.string.theme_sailor_name) -> R.style.Theme_Sailor
-            resources.getString(R.string.theme_royal_name) -> R.style.Theme_Royal
-            resources.getString(R.string.theme_mercury_name) -> R.style.Theme_Mercury
-            resources.getString(R.string.theme_mocca_name) -> R.style.Theme_Mocca
-            resources.getString(R.string.theme_creeper_name) -> R.style.Theme_Creeper
-            resources.getString(R.string.theme_flamingo_name) -> R.style.Theme_Flamingo
-            resources.getString(R.string.theme_pilot_name) -> R.style.Theme_Pilot
-            resources.getString(R.string.theme_coral_name) -> R.style.Theme_Coral
-            resources.getString(R.string.theme_blossom_name) -> R.style.Theme_Blossom
+            resources.getString(R.string.theme_default) -> R.style.Theme_Default
+            resources.getString(R.string.theme_sailor) -> R.style.Theme_Sailor
+            resources.getString(R.string.theme_royal) -> R.style.Theme_Royal
+            resources.getString(R.string.theme_mercury) -> R.style.Theme_Mercury
+            resources.getString(R.string.theme_mocca) -> R.style.Theme_Mocca
+            resources.getString(R.string.theme_creeper) -> R.style.Theme_Creeper
+            resources.getString(R.string.theme_flamingo) -> R.style.Theme_Flamingo
+            resources.getString(R.string.theme_pilot) -> R.style.Theme_Pilot
+            resources.getString(R.string.theme_coral) -> R.style.Theme_Coral
+            resources.getString(R.string.theme_blossom) -> R.style.Theme_Blossom
             else -> R.style.Theme_Default
         }
     }
